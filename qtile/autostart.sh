71a482e3be024fa7b97abed12b211b857dcc6bc0
Null <datguykenkai@pop-os.localdomain>
2021-04-24 11:52:43 -0400

    added dotfiles

---
+++ qtile/autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#nitrogen --restore &
+wal -R
+pkill compton
+compton --config ~/.config/compton.conf &
+#compton &
+vmware-user
+#glava
+~/.config/wpg/wp_init.sh
---
+++ wpg/wp_init.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wpg -rs 'ILb6j1i.png' 'ILb6j1i.png'
